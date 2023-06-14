562f7a82a45a98f830651cfe0e5ee4e3218510ee
Amna karim <amnakarim0005@gmail.com>
2023-06-14 19:53:12 +0500

    header section

---
+++ js/index.js
@@ -0,0 +1,8 @@
+// let nav = document.querySelector("nav");
+//     window.onscroll = function() {
+//       if(document.documentElement.scrollTop > 20){
+//         nav.classList.add("sticky");
+//       }else {
+//         nav.classList.remove("sticky");
+//       }
+//     }
